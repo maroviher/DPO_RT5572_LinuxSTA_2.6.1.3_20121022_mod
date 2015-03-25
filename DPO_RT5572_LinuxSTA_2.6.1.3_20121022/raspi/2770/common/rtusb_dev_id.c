6f39cfe14c3f02cca68645a1b841a038d4082b12
maroviher <qscgy@mail.ru>
2015-03-25 17:01:11 +0100

    added raspberry pi customizations

---
+++ DPO_RT5572_LinuxSTA_2.6.1.3_20121022/raspi/2770/common/rtusb_dev_id.c
@@ -0,0 +1,43 @@
+/*
+ *************************************************************************
+ * Ralink Tech Inc.
+ * 5F., No.36, Taiyuan St., Jhubei City,
+ * Hsinchu County 302,
+ * Taiwan, R.O.C.
+ *
+ * (c) Copyright 2002-2010, Ralink Technology, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                       *
+ *************************************************************************/
+
+
+#define RTMP_MODULE_OS
+
+/*#include "rt_config.h"*/
+#include "rtmp_comm.h"
+#include "rt_os_util.h"
+#include "rt_os_net.h"
+
+
+/* module table */
+USB_DEVICE_ID rtusb_dev_id[] = {
+	{USB_DEVICE(0x148F,0x3070)}, /* Proware */
+	{ }/* Terminating entry */
+};
+
+INT const rtusb_usb_id_len = sizeof(rtusb_dev_id) / sizeof(USB_DEVICE_ID);
+MODULE_DEVICE_TABLE(usb, rtusb_dev_id);
---
+++ DPO_RT5572_LinuxSTA_2.6.1.3_20121022/raspi/cross_compile.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ $# != 3 ]
+then
+  echo "Usage: $0 CHIP CC_PREFIX LINUX_SRC"
+  echo "Example: $0 3070 arm-linux-gnueabihf- /media/root/967cec13-9afa-45fb-b861-cb3255f6275d/rpi_kernel_23.03.2015/linux"
+  exit 2
+fi
+
+#checking if cross compiler found
+if which $2gcc > /dev/null
+then
+  echo "Crosscompiler found"
+else
+  echo "cross compiler whith prefix $2 not found in your \$PATH. Do you need something like \"export PATH=\$PATH:/home/ahmed/raspi_toolchains/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin\"?"
+  exit 2
+fi
+
+chips="2770 3070 5270"
+for chip in $chips
+do
+  if [ $1 -eq $chip ]
+  then
+    target_chip=$chip;
+    break
+  fi
+done
+
+if test -z "$target_chip"
+then
+  echo "Error, specify chipset in first parameter, only this chips are supported: $chips"
+  exit 1
+fi
+
+FILES="include/os/rt_drv.h include/os/rt_linux.h include/rtmp_def.h common/rtusb_dev_id.c"
+for f in $FILES; do
+  mv ../$f ../${f}_org
+  ln -s `pwd`/$target_chip/$f ../$f
+done
+
+cd ..
+CROSS_COMPILE=$2 LINUX_SRC=$3 make -j4
+
+cd -
+for f in $FILES; do
+  rm ../$f
+  mv ../${f}_org ../$f
+done
